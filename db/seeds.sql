5510aa74b87f84fbc04ae343a97dc384beaf3531
Charlotte Mountain <charr.mt@gmail.com>
2020-04-15 11:51:38 -0700

    inital commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Bacon Cheeseburger', false);
