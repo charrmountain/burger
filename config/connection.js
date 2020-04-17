66645dbdab72ac50c3bea66687220b79eff99102
Charlotte Mountain <charr.mt@gmail.com>
2020-04-17 14:10:51 -0700

    change?

--- config/connection.js
+++ config/connection.js
@@ -13,25 +13,18 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection;
-// = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "burgers_db",
-// });
-
-if (process.env.JAWSDB_URL){
+let connection;
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}else{
+} else {
   connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 's1jhdrm943nanv0l'
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
   });
-};
+}
 
 connection.connect((err) => {
   if (err) {
