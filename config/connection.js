f964f92ab3bc49df231d799a8ccbb334bf3ff3f3
Charlotte Mountain <charr.mt@gmail.com>
2020-04-17 12:15:44 -0700

    heroku

--- config/connection.js
+++ config/connection.js
@@ -14,6 +14,13 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 const connection;
+// = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db",
+// });
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
